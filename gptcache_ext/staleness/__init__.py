4c7af9a118e47ea5608525c3d5e6ea52dd9177e5
LiorLotan2 <liorlotan2@gmail.com>
2026-07-25 21:06:38 +0300

    Add __all__ to staleness package init to satisfy flake8

--- gptcache_ext/staleness/__init__.py
+++ gptcache_ext/staleness/__init__.py
@@ -1,3 +1,10 @@
-from gptcache_ext.staleness.clusters import assign_cluster, fit_clusters  # A3
-from gptcache_ext.staleness.fitter import fit_staleness_table              # A3
-from gptcache_ext.staleness.gate import TTLGate                           # A3
+from gptcache_ext.staleness.clusters import assign_cluster, fit_clusters
+from gptcache_ext.staleness.fitter import fit_staleness_table
+from gptcache_ext.staleness.gate import TTLGate
+
+__all__ = [
+    "assign_cluster",
+    "fit_clusters",
+    "fit_staleness_table",
+    "TTLGate",
+]
